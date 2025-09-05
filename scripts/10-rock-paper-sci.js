a947c09f3592560661c1641f0b991c403725c957
Shreeya Garje <shreeyagarje08@gmail.com>
2025-09-05 16:12:58 +0530

    Add files via upload

--- scripts/10-rock-paper-sci.js
+++ scripts/10-rock-paper-sci.js
@@ -1,1 +1,100 @@
+// When we first load the page this happens 
+let score = JSON.parse(localStorage.getItem('score')) || {
+  wins: 0,
+  losses: 0,
+  ties: 0
+}
+/*
+above code is the shortcut(default operator) for the code below
+!score is same as score === null
+if(!score){
+  score = {
+    wins: 0,
+    losses: 0,
+    ties: 0
+  };
+}
+*/
 
+updateScoreElement();
+// So that it is visible on screen
+
+
+function playGame(playerMove){
+  
+  const computerMove = pickComputerMove();
+
+  let result = '';
+  if (playerMove === 'scissors'){
+    if(computerMove === 'rock'){
+      result = 'You lose.';
+    } else if (computerMove === 'paper') {
+      result = 'You win!';
+    } else if(computerMove === 'scissors') {
+      result = 'Tie.';
+    }
+
+  } else if (playerMove === 'paper'){
+    if(computerMove === 'rock'){
+      result = 'You win!';
+    } else if (computerMove === 'paper') {
+      result = 'Tie.';
+    } else if(computerMove === 'scissors') {
+      result = 'You lose.';
+    }
+
+  } else if (playerMove === 'rock'){
+    if(computerMove === 'rock'){
+      result = 'Tie.';
+    } else if (computerMove === 'paper') {
+      result = 'You lose.';
+    } else if(computerMove === 'scissors') {
+      result = 'You win!';
+    }
+  }
+
+  if(result === 'You win!'){
+    score.wins += 1;
+  } else if(result === 'You lose.'){
+    score.losses += 1;
+  } else if (result === "Tie.") {
+    score.ties +=1;
+  }
+
+
+  localStorage.setItem('score', JSON.stringify(score));
+  //LocalStorage only supports strings so we have to convert our score object into string using the JSON object.
+
+  updateScoreElement();
+  // So that is is updated
+
+  document.querySelector('.js-result').innerHTML = result;
+
+  document.querySelector('.js-moves').innerHTML =`You
+  <img src="images/${playerMove}-emoji.png" class="move-icon">
+  <img src="images/${computerMove}-emoji.png" class="move-icon">
+  Computer`;
+
+  alert(`You picked ${playerMove}. Computer picked ${computerMove}. ${result}\nWins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`);
+}
+
+function updateScoreElement() {
+    document.querySelector('.js-score').innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+}
+
+function pickComputerMove() {
+
+  const randomNumber = Math.random();
+
+  let computerMove = '';
+
+  if(randomNumber >= 0 && randomNumber < 1/3){
+    computerMove = 'rock';
+  } else if(randomNumber >= 1/3 && randomNumber < 2/3){
+    computerMove = 'paper';
+  } else if(randomNumber >= 2/3 && randomNumber < 1){
+    computerMove = 'scissors';
+  } 
+
+  return computerMove;
+}
